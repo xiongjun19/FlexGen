a2eca655006b25f4621ebecd8715c9bbba5356ee
JunXiong <jun.xiong@lightelligence.ai>
2023-03-09 17:32:43 +0800

    adding batch run

---
+++ scripts/batch_run.sh
@@ -0,0 +1,32 @@
+log_dir=/workspace/data/flex_logs
+mkdir -p ${log_dir}
+gpu_logs=${log_dir}/gpu_logs
+cpu_logs=${log_dir}/cpu_logs
+mkdir -p ${gpu_logs}
+mkdir -p ${cpu_logs}
+
+model_prefix="facebook"
+# model_name_arr=("opt-66b" "opt-175b")
+model_name_arr=("opt-66b")
+input_len_arr=(512 1024)
+out_len_arr=(128 256 512 1024)
+bs=4
+
+for(( i=0;i<${#model_name_arr[@]};i++)) do
+   for(( j=0;j<${#input_len_arr[@]};j++)) do
+     for(( k=0;k<${#out_len_arr[@]};k++)) do
+         model_name=${model_name_arr[i]};
+         model_path=${model_prefix}/${model_name};
+         input_len=${input_len_arr[j]};
+	 out_len=${out_len_arr[k]};
+	 gpu_log="${gpu_logs}/${model_name}_${input_len}_${out_len}.qdrep";
+         cpu_log="${cpu_logs}/${model_name}_${input_len}_${out_len}.txt";
+	 cmd="nsys profile  -c cudaProfilerApi -f true --stats true  -o ${prof_log} python flex_opt.py --model ${model_path} --path _DUMMY_  --percent 0 0 0 0 100 0 --gpu-batch-size ${bs} --num-gpu-batches 8  --prompt-len ${input_len} --gen-len ${out_len} --cpu_log_path ${cpu_log} ";
+	 echo $cmd; 
+         # cmd;	
+	 echo "done";
+      done
+    done
+ done
+
+
