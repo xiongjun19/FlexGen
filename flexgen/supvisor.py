a260905a40dba9411357704157bbfec194ecaef7
Jun Xiong <jun.xiong@lightelligence.ai>
2023-07-09 17:33:49 +0800

    fixing bug in supervisor

--- flexgen/supvisor.py
+++ flexgen/supvisor.py
@@ -7,19 +7,31 @@ import argparse
 import subprocess
 import shlex
 from subprocess import Popen
+import psutil
 
 
 def exe_cmd_sync(cmd_line):
     # call is blocking:
+    print(cmd_line)
     cmd_args = shlex.split(cmd_line)
     subprocess.call(cmd_args)
 
 
+# def kill_process(key_word):
+#     cmd = f"ps aux | grep flex | grep opt | grep {key_word} | " 
+#     cmd_part2 = 'awk \'{print $2}\' | xargs kill -9'
+#     cmd_line = cmd + cmd_part2
+#     exe_cmd_sync(cmd_line)
+
+
 def kill_process(key_word):
-    cmd = f"ps aux | grep flex | grep opt | grep {key_word} | " 
-    cmd_part2 = 'awk \'{print $2}\' | xargs kill -9'
-    cmd_line = cmd + cmd_part2
-    exe_cmd_sync(cmd_line)
+    for proc in psutil.process_iter():
+        pinfo = proc.as_dict(attrs=['pid', 'cmdline'])
+        
+        if any( key_word in x for x in pinfo['cmdline']):
+            print("find kill proc successful")
+            proc.kill()
+
 
 
 def get_lines(in_file, line_no):
@@ -41,17 +53,20 @@ def get_keyword(lines):
         if "starting collect file" in line:
             start_no = num
             key = line.strip().split(":")[1].strip()
+        num = num + 1
     return key, start_no
 
 
 def _need_kill(lines, key, start_no):
+    print("to find weather need kill")
     if key is None:
         return False
     num = 0
     for line in lines:
         if num > start_no:
-            if 'error' in line.lower():
+            if 'error' in line.lower() and 'inotify_add_watch' not in line.lower():
                 return True
+        num += 1
     return False
 
 
