67a14b1ceadf163edb0028e43743b8b18f5ab559
azharlightelligence <91244718+azharlightelligence@users.noreply.github.com>
2023-07-13 21:53:13 +0800

    Create main.py
    Test bandwidths of GPU-MEM

---
+++ flexgen/bandwidth_test/main.py
@@ -0,0 +1,44 @@
+import subprocess
+import csv
+import time
+import os
+
+
+data_sizes = [4 * 1024, 16 * 1024, 64 * 1024, 256 * 1024, 1024 * 1024, 4 * 1024 * 1024, 16 * 1024 * 1024, 64 * 1024 * 1024, 256 * 1024 * 1024]
+
+
+output_file = 'output.csv'
+
+# Write the CSV header
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(['data_size_KB','device_id','device_type' ,'mem_type', 'memory_to_device_GB/s', 'device_to_memory_GB/s'])
+
+mem_type = ['mem0','mem1','cxl']
+
+device_ids =[f"{i}" for i in range(8)]
+
+for device_id in device_ids:
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{device_id}"
+    print(f"Testing for Device ID: {device_id}")
+    for idx,mem in enumerate(mem_type):
+        
+        # Run the command for each data size
+        for size in data_sizes:
+            command = f"numactl --membind={idx} ./profile_test --mem-type={mem} --data-size={size}"
+            result = subprocess.run(command, capture_output=True, text=True, shell=True)
+            output = result.stdout.strip()
+            
+            print(output)
+            # Parse the relevant values from the command output
+            lines = output.split('\n')
+            transfer1_bandwidth = lines[-2].split(':')[1].strip()
+            transfer2_bandwidth = lines[-1].split(':')[1].strip()
+            device_type = lines[2].strip('Device: ')
+            # Append the data to the CSV file
+            with open(output_file, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([size/1024,device_id,device_type,mem, transfer1_bandwidth, transfer2_bandwidth])
+            time.sleep(10)
+print("Output saved to", output_file)
