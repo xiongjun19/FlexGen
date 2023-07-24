6ce31ec058d3199bf5dcf53ded0eb90e81801260
azharlightelligence <91244718+azharlightelligence@users.noreply.github.com>
2023-07-24 23:20:27 +0800

    Create run-demo.sh
    Script to run the demo with CXL run as default

---
+++ flexgen/run-demo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Set the number of iterations (use -1 for infinite loop)
+COUNT=10
+
+# Function to run the rundk-large.sh script
+run_script() {
+    ./rundk-large.sh --cxl-offload
+}
+
+# Infinite while loop when COUNT is -1, or run COUNT times
+if [ "$COUNT" -eq -1 ]; then
+    echo "Running $RUN_SCRIPT script infinitely..."
+    while true; do
+        run_script
+        sleep 5
+    done
+else
+    echo "Running $RUN_SCRIPT script $COUNT times..."
+    for ((i = 1; i <= COUNT; i++)); do
+        run_script
+        sleep 5
+    done
+fi
